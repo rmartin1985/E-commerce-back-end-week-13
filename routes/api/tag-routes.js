9e427da2626461c3f35f5166d120cd1f29da4e73
Rick Martin <rmartin1985@att.net>
2021-11-07 16:37:36 -0600

    tag get all route created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { findAll } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
 });
 
 router.get('/:id', (req, res) => {
